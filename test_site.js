38d62e50bbd060d6a407c80f6b371ff954f94713
Navya Eetaram <navyaeetaram17@gmail.com>
2016-01-31 18:21:13 -0600

    update

--- test_site.js
+++ test_site.js
@@ -1,6 +1,7 @@
-var me = document.querySelector('html')
+/*var me = document.querySelector('html')
 me.onclick = function() {
     var myname= prompt('please enter you name');
     localStorage.setItem('name',myname);
     me.textContent= myname +' is cool';
-};
+};*/
+var ds=["Arrays","Lists","Stack","Queues","Linked lists","Dictionaries","Hashing","sets","Binary Trees","Graphs","Algorithms","Dynamic programming"]
